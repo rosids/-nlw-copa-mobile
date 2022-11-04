5e9469f31763e43286735dd1004a8ecc66b78bff
Rosiele David <rosieleserpa@hotmail.com>
2022-11-04 03:17:16 -0300

    inicia implementação de autenticação no google

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,4 +1,9 @@
-import { createContext, ReactNode, useState } from "react";
+import { createContext, ReactNode, useEffect, useState } from "react";
+import * as Google from 'expo-auth-session/providers/google';
+import * as AuthSession from 'expo-auth-session';
+import * as WebBrowser from 'expo-web-browser';
+
+WebBrowser.maybeCompleteAuthSession(); // redireciona do navegador para o aplicativo novamente
 
 interface UserProps {
   name: string;
@@ -7,6 +12,7 @@ interface UserProps {
 
 export interface AuthContextDataProps {
   user: UserProps;
+  isUserLoading: boolean;
   signIn: () => Promise<void>;
 }
 
@@ -18,14 +24,43 @@ export const AuthContext = createContext({} as AuthContextDataProps);
 
 export function AuthContextProvider({ children }: AuthProviderProps) {
   const [user, setUser] = useState<UserProps>({} as UserProps);
+  const [isUserLoading, setIsUserLoading] = useState(false);
+
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    clientId: '', // clienteId da aplicação que foi cadastrada no console.cloud.google.com
+    redirectUri: AuthSession.makeRedirectUri({ useProxy: true }), // link de redirecionamento da aplicação
+    scopes: ['profile', 'email'], // o que será acessado do usuário
+  });
 
   async function signIn() {
-    console.log('Vamos logar!');
+    try {
+      setIsUserLoading(true);
+      promptAsync(); // permite iniciar o fluxo de autenticação no google
+    } catch (error) {
+      console.log(error);
+      throw error;
+    } finally {
+      setIsUserLoading(false);
+    }
   }
+
+  async function signInWithGoogle(access_token: string) {
+    console.log('TOKEN DE AUTENTICAÇÃO =>', access_token);
+  }
+
+  useEffect(() => {
+    // response => resposta da autenticação no google
+    // verifica se o tipo da resposta é success e se dentro da resposta tem o objeto authentication
+    // e dentro dele tem o accessToken
+    if(response?.type === 'success' && response.authentication?.accessToken) {
+      signInWithGoogle(response.authentication.accessToken);
+    }
+  }, [response]);
 
   return (
     <AuthContext.Provider value={{
       user,
+      isUserLoading,
       signIn,
     }}>
       {children}
