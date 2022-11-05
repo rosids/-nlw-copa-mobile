eb4ed2cb93f8fadd1335215df2f39f3a37f8fedf
Rosiele David <rosieleserpa@hotmail.com>
2022-11-05 18:14:03 -0300

    conecta api com a tela de bolões

--- src/screens/Pools.tsx
+++ src/screens/Pools.tsx
@@ -1,12 +1,46 @@
-import { Icon, VStack } from "native-base";
+import { useCallback, useEffect, useState } from 'react';
+import { FlatList, Icon, useToast, VStack } from 'native-base';
 import { Octicons } from '@expo/vector-icons';
-import { useNavigation } from '@react-navigation/native';
+import { useFocusEffect, useNavigation } from '@react-navigation/native';
 
-import { Button } from "../components/Button";
-import { Header } from "../components/Header";
+import { api } from '../services/api';
+
+import { Button } from '../components/Button';
+import { Header } from '../components/Header';
+import { Loading } from '../components/Loading';
+import { EmptyPoolList } from '../components/EmptyPoolList';
+import { PoolCard, PoolCardProps } from '../components/PoolCard';
 
 export function Pools() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [pools, setPools] = useState<PoolCardProps[]>([]);
+
   const { navigate } = useNavigation();
+  const toast = useToast();
+
+  async function fetchPools() {
+    try {
+      setIsLoading(true);
+
+      const response = await api.get('/pools');
+      setPools(response.data.pools);
+
+    } catch (error) {
+      console.log(error);
+
+      toast.show({
+        title: 'Não foi possível carregar os bolões.',
+        placement: 'top',
+        bgColor: 'red.500',
+      });
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  useFocusEffect(useCallback(() => { // useFocusEffect garante que vai ser feito a chamada da api sempre que a tela estiver em foco
+    fetchPools();
+  }, []));
 
   return (
     <VStack flex={1} bgColor="gray.900">
@@ -19,6 +53,20 @@ export function Pools() {
           onPress={() => navigate('find')}
         />
       </VStack>
+
+      {
+        isLoading ? <Loading /> : (
+          <FlatList
+            data={pools}
+            keyExtractor={item => item.id}
+            renderItem={({ item }) => <PoolCard data={item} />} // informa qual componente deve ser exibido
+            ListEmptyComponent={() => <EmptyPoolList />} // exibe esse componente quando não há bolões
+            showsVerticalScrollIndicator={false} // remove scroll vertical
+            _contentContainerStyle={{ pb: 10 }} // add espaçamento embaixo ao rolar a listagem
+            px={5} // add padding lateral
+          />
+        )
+      }
     </VStack>
   );
 }
