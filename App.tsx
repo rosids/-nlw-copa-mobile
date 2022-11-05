f6263b48f6b06a995da483c43274d0ba6888a324
Rosiele David <rosieleserpa@hotmail.com>
2022-11-05 13:21:39 -0300

    add rotas e suas estilizações

--- App.tsx
+++ App.tsx
@@ -1,9 +1,9 @@
-import { NativeBaseProvider, StatusBar } from "native-base";
+import { NativeBaseProvider, StatusBar } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
-import { AuthContextProvider } from "./src/contexts/AuthContext";
+import { AuthContextProvider } from './src/contexts/AuthContext';
 
-import { SignIn } from './src/screens/SignIn';
+import { Routes } from './src/routes';
 import { Loading } from './src/components/Loading';
 
 import { THEME } from './src/styles/theme';
@@ -20,7 +20,7 @@ export default function App() {
           translucent
         />
 
-        { fontsLoaded  ? <SignIn /> : <Loading />}
+        { fontsLoaded  ? <Routes /> : <Loading />}
       </AuthContextProvider>
     </NativeBaseProvider>
   );
