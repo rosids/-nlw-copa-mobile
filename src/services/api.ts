a5d60ec4919078cc3fe62af626f309e50fc23905
Rosiele David <rosieleserpa@hotmail.com>
2022-11-05 16:44:57 -0300

    add axios e configura axios no projeto

---
+++ src/services/api.ts
@@ -0,0 +1,7 @@
+import axios from 'axios';
+
+export const api = axios.create({
+  // quando for rodar o servidor precisa definir o ip da máquina(NÃO PODE SER LOCALHOST)
+  // OBS: ISSO SÓ NO AMBIENTE DE DESENVOLVIMENTO
+  baseURL: 'http://ip-da-maquina:3333'
+});
