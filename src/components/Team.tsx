0977cd3b995955b3f7f02dd8c1e8e52d2407328b
Rosiele David <rosieleserpa@hotmail.com>
2022-11-05 21:45:40 -0300

    add campo value no componente Team

--- src/components/Team.tsx
+++ src/components/Team.tsx
@@ -7,9 +7,10 @@ interface Props {
   code: string;
   position: 'left' | 'right';
   onChangeText: (value: string) => void;
+  value: string;
 }
 
-export function Team({ code, position, onChangeText }: Props) {
+export function Team({ code, position, onChangeText, value }: Props) {
   return (
     <HStack alignItems="center">
       {position === 'left' && <CountryFlag isoCode={code} size={25} style={{ marginRight: 12 }} />}
@@ -21,6 +22,7 @@ export function Team({ code, position, onChangeText }: Props) {
         fontSize="xs"
         keyboardType="numeric"
         onChangeText={onChangeText}
+        value={value}
       />
 
       {position === 'right' && <CountryFlag isoCode={code} size={25} style={{ marginLeft: 12 }} />}
